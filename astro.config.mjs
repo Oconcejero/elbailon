6db0a5cd7956850a84e68a5ce318c0d1792bc97e
Oconcejero <oconcejero@gmail.com>
2026-03-09 15:03:46 +0100

    primer commit pagina el bailon

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // Esta es la URL de navegación, no la de repositorio
+  site: 'https://Oconcejero.github.io',
+
+  // El nombre del repo está perfecto
+  base: '/elbailon',
+
+  trailingSlash: 'always',
+});
