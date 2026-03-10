ef27d07c467be133df1fce1a4816c8cc38466b2a
Oconcejero <oconcejero@gmail.com>
2026-03-10 14:06:52 +0100

    Cambios finales página el bailon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Esta es la URL de navegación, no la de repositorio
+
+  base: process.env.NODE_ENV === 'production' ? '/elbailon' : '/',
   site: 'https://Oconcejero.github.io',
-
-  // El nombre del repo está perfecto
-  base: '/elbailon',
-
   trailingSlash: 'always',
 });
